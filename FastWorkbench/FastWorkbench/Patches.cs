5322209bbb224279793f4015a73a27616e7e5e99
WiseHorror <diogo.garrett93@gmail.com>
2024-10-16 22:18:07 +0100

    FastWorkbench - Fixed for 0.47.x - Changed max limits to 20000 - Added minimum price limit setting

--- FastWorkbench/FastWorkbench/Patches.cs
+++ FastWorkbench/FastWorkbench/Patches.cs
@@ -1,5 +1,8 @@
 ﻿using HarmonyLib;
+using I2.Loc;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace FastWorkbench
@@ -9,7 +12,7 @@ namespace FastWorkbench
     {
         [HarmonyPrefix]
         [HarmonyPatch(typeof(InteractableWorkbench), nameof(InteractableWorkbench.PlayBundlingCardBoxSequence))]
-        private static bool InteractableWorkbench_PlayBundlingCardBoxSequence_Prefix(ref InteractableWorkbench __instance, List<CardData> cardDataListShown, ECardExpansionType cardExpansionType)
+        public static bool InteractableWorkbench_PlayBundlingCardBoxSequence_Prefix(ref InteractableWorkbench __instance, List<CardData> cardDataListShown, ECardExpansionType cardExpansionType, float totalPrice)
         {
             if (!Plugin.EnableMod.Value)
             {
@@ -35,15 +38,16 @@ namespace FastWorkbench
             }
 
             __instance.m_JankBoxAnim.speed = Plugin.SpeedMultiplier.Value;
-
-            __instance.m_JankBoxAnim.gameObject.SetActive(true);
-            __instance.m_JankBoxAnim.SetBool("IsClosing", true);
-            ItemMeshData itemMeshData = InventoryBase.GetItemMeshData(__instance.GetBulkBoxItemType(cardExpansionType));
+            __instance.m_JankBoxAnim.gameObject.SetActive(value: true);
+            __instance.m_JankBoxAnim.SetBool("IsClosing", value: true);
+            __instance.m_CurrentItemTypeSpawn = __instance.GetBulkBoxItemType(cardExpansionType, totalPrice);
+            ItemMeshData itemMeshData = InventoryBase.GetItemMeshData(__instance.m_CurrentItemTypeSpawn);
             __instance.m_JankBoxSkinMesh.material = itemMeshData.material;
             for (int i = 0; i < cardDataListShown.Count; i++)
             {
                 __instance.m_InteractableCard3dList[i].m_Card3dUI.m_CardUI.SetCardUI(cardDataListShown[i]);
             }
+
             __instance.m_IsPlayCardEnterAnim = true;
 
             return false;
@@ -51,7 +55,7 @@ namespace FastWorkbench
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(InteractableWorkbench), nameof(InteractableWorkbench.OnMouseButtonUp))]
-        private static bool InteractableWorkbench_OnMouseButtonUp_Prefix(ref InteractableWorkbench __instance)
+        public static bool InteractableWorkbench_OnMouseButtonUp_Prefix(ref InteractableWorkbench __instance)
         {
             if (!Plugin.EnableMod.Value)
             {
@@ -71,7 +75,7 @@ namespace FastWorkbench
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(WorkbenchUIScreen), nameof(WorkbenchUIScreen.Update))]
-        private static bool WorkbenchUIScreen_Update_Prefix(ref WorkbenchUIScreen __instance)
+        public static bool WorkbenchUIScreen_Update_Prefix(ref WorkbenchUIScreen __instance)
         {
             if (!Plugin.EnableMod.Value)
             {
@@ -97,161 +101,92 @@ namespace FastWorkbench
 
         [HarmonyPrefix]
         [HarmonyPatch(typeof(WorkbenchUIScreen), nameof(WorkbenchUIScreen.OnSliderValueChanged_PriceLimit))]
-        private static bool WorkbenchUIScreen_OnSliderValueChanged_PriceLimit_Prefix(ref WorkbenchUIScreen __instance)
+        public static bool WorkbenchUIScreen_OnSliderValueChanged_PriceLimit_Prefix(ref WorkbenchUIScreen __instance)
         {
             if (!Plugin.EnableMod.Value)
             {
                 return true;
             }
 
-            if (__instance.m_IgnoreSliderUpdateFunction)
+            if (!__instance.m_IgnoreSliderUpdateFunction)
             {
-                return false;
+                __instance.m_PriceLimit = (float)Mathf.RoundToInt(__instance.m_SliderPriceLimit.value) / 100f;
+                __instance.m_PriceLimitText.text = GameInstance.GetPriceString(__instance.m_PriceLimit);
+                CPlayerData.m_WorkbenchPriceLimit = __instance.m_PriceLimit;
             }
-            __instance.m_PriceLimit = (float)Mathf.RoundToInt(__instance.m_SliderPriceLimit.value) / 100f;
-            __instance.m_PriceLimitText.text = GameInstance.GetPriceString(__instance.m_PriceLimit, false, true, false, "F2");
-            CPlayerData.m_WorkbenchPriceLimit = __instance.m_PriceLimit;
 
             return false;
         }
 
         [HarmonyPrefix]
+        [HarmonyPatch(typeof(WorkbenchUIScreen), nameof(WorkbenchUIScreen.OnSliderValueChanged_PriceMinimum))]
+        public static bool WorkbenchUIScreen_OnSliderValueChanged_PriceMinimum_Prefix(ref WorkbenchUIScreen __instance)
+        {
+            if (!Plugin.EnableMod.Value)
+            {
+                return true;
+            }
+
+            if (!__instance.m_IgnoreSliderUpdateFunction)
+            {
+                __instance.m_PriceMinimum = (float)Mathf.RoundToInt(__instance.m_SliderPriceMinimum.value) / 100f;
+                __instance.m_PriceMinimumText.text = GameInstance.GetPriceString(__instance.m_PriceMinimum);
+                CPlayerData.m_WorkbenchPriceMinimum = __instance.m_PriceMinimum;
+            }
+
+            return false;
+        }
+
+        [HarmonyPostfix]
         [HarmonyPatch(typeof(WorkbenchUIScreen), nameof(WorkbenchUIScreen.OpenScreen))]
-        private static bool WorkbenchUIScreen_OpenScreen_Prefix(InteractableWorkbench interactableWorkbench)
+        public static void WorkbenchUIScreen_OpenScreen_Postfix(InteractableWorkbench interactableWorkbench)
         {
             if (!Plugin.EnableMod.Value)
             {
-                CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceLimit.maxValue = 1f;
-                return true;
+                CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceLimit.maxValue = CPlayerData.m_WorkbenchPriceLimit * 100f;
+                CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceMinimum.value = CPlayerData.m_WorkbenchPriceMinimum * 100f;
+                return;
             }
 
+            CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceLimit.minValue = Plugin.MinimumPriceLimit.Value * 100f;
             CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceLimit.maxValue = Plugin.MaxPriceLimit.Value * 100f;
-
-            if (CPlayerData.m_WorkbenchPriceLimit > 0f)
-            {
-                CSingleton<WorkbenchUIScreen>.Instance.m_MinimumCardLimit = CPlayerData.m_WorkbenchMinimumCardLimit;
-                CSingleton<WorkbenchUIScreen>.Instance.m_PriceLimit = CPlayerData.m_WorkbenchPriceLimit;
-                CSingleton<WorkbenchUIScreen>.Instance.m_RarityLimit = CPlayerData.m_WorkbenchRarityLimit;
-                CSingleton<WorkbenchUIScreen>.Instance.m_CurrentCardExpansionType = CPlayerData.m_WorkbenchCardExpansionType;
-            }
-            CSingleton<WorkbenchUIScreen>.Instance.m_IgnoreSliderUpdateFunction = true;
-            CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceLimit.value = CPlayerData.m_WorkbenchPriceLimit * 100f;
-            CSingleton<WorkbenchUIScreen>.Instance.m_SliderMinCard.value = (float)CPlayerData.m_WorkbenchMinimumCardLimit / 10f * 10f;
-            CSingleton<WorkbenchUIScreen>.Instance.m_PriceLimitMinText.text = GameInstance.GetPriceString(0.01f, false, true, false, "F2");
-            CSingleton<WorkbenchUIScreen>.Instance.m_PriceLimitMaxText.text = GameInstance.GetPriceString(Plugin.MaxPriceLimit.Value, false, true, false, "F2");
-            CSingleton<WorkbenchUIScreen>.Instance.m_IgnoreSliderUpdateFunction = false;
-            CSingleton<WorkbenchUIScreen>.Instance.m_PriceLimitText.text = GameInstance.GetPriceString(CSingleton<WorkbenchUIScreen>.Instance.m_PriceLimit, false, true, false, "F2");
-            CSingleton<WorkbenchUIScreen>.Instance.m_MinimumCardText.text = CSingleton<WorkbenchUIScreen>.Instance.m_MinimumCardLimit.ToString();
-            CSingleton<WorkbenchUIScreen>.Instance.m_TaskFinishCirlceGrp.SetActive(false);
-            CSingleton<WorkbenchUIScreen>.Instance.m_CurrentInteractableWorkbench = interactableWorkbench;
-            CSingleton<WorkbenchUIScreen>.Instance.m_ScreenGrp.SetActive(true);
-            SoundManager.GenericMenuOpen(1f, 1f);
-            ControllerScreenUIExtManager.OnOpenScreen(CSingleton<WorkbenchUIScreen>.Instance.m_ControllerScreenUIExtension);
-            TutorialManager.SetGameUIVisible(false);
-
-            return false;
+            CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceMinimum.minValue = Plugin.MinimumPriceLimit.Value * 100f;
+            CSingleton<WorkbenchUIScreen>.Instance.m_SliderPriceMinimum.maxValue = Plugin.MaxPriceLimit.Value * 100f;
+            CSingleton<WorkbenchUIScreen>.Instance.m_PriceLimitMinText.text = GameInstance.GetPriceString(Plugin.MinimumPriceLimit.Value);
+            CSingleton<WorkbenchUIScreen>.Instance.m_PriceLimitMaxText.text = GameInstance.GetPriceString(Plugin.MaxPriceLimit.Value);
+            CSingleton<WorkbenchUIScreen>.Instance.m_PriceMinimumMinText.text = GameInstance.GetPriceString(Plugin.MinimumPriceLimit.Value);
+            CSingleton<WorkbenchUIScreen>.Instance.m_PriceMinimumMaxText.text = GameInstance.GetPriceString(Plugin.MaxPriceLimit.Value);
+            return;
         }
 
-        [HarmonyPrefix]
+        [HarmonyTranspiler]
         [HarmonyPatch(typeof(InteractableWorkbench), nameof(InteractableWorkbench.Update))]
-        private static bool InteractableWorkbench_Update_Prefix(ref InteractableWorkbench __instance)
+        public static IEnumerable<CodeInstruction> InteractableWorkbench_Update_Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            if (!Plugin.EnableMod.Value)
-            {
-                return true;
-            }
+            var codes = new List<CodeInstruction>(instructions);
 
-            __instance = InteractableObjectUpdate(__instance);
-            if (__instance.m_IsPlayCardEnterAnim)
-            {
-                __instance.m_CardEnterTimer += Time.deltaTime * Plugin.SpeedMultiplier.Value;
-                if (__instance.m_CardEnterTimer > 0.1f)
-                {
-                    __instance.m_CardEnterBoxAnimList[__instance.m_CardEnterIndex].gameObject.SetActive(false);
-                    __instance.m_CardEnterBoxAnimList[__instance.m_CardEnterIndex].gameObject.SetActive(true);
-                    __instance.m_InteractableCard3dList[__instance.m_CardEnterIndex].m_Card3dUI.SetVisibility(true);
-                    __instance.m_CardEnterBoxAnimList[__instance.m_CardEnterIndex].Play();
-                    __instance.m_CardEnterTimer = 0f;
-                    __instance.m_CardEnterIndex++;
-                    if (__instance.m_CardEnterIndex >= __instance.m_InteractableCard3dList.Count)
-                    {
-                        __instance.m_IsPlayCardEnterAnim = false;
-                        __instance.m_CardEnterIndex = 0;
-                    }
-                }
-            }
+            MethodInfo deltaTimeGetter = AccessTools.Property(typeof(Time), nameof(Time.deltaTime)).GetGetMethod();
+            MethodInfo speedMultiplierGetter = AccessTools.Property(typeof(Plugin), nameof(Plugin.SpeedMultiplierValue)).GetGetMethod();
 
-            return false;
-        }
-
-        public static InteractableWorkbench InteractableObjectUpdate(InteractableWorkbench interactableWorkbench)
-        {
-            if (interactableWorkbench.m_IsLerpingToPos)
+            for (int i = 0; i < codes.Count; i++)
             {
-                interactableWorkbench.m_LerpPosTimer += Time.deltaTime * interactableWorkbench.m_LerpPosSpeed;
-                interactableWorkbench.transform.position = Vector3.Lerp(interactableWorkbench.m_StartLerpPos, interactableWorkbench.m_TargetLerpTransform.position, interactableWorkbench.m_LerpPosTimer);
-                interactableWorkbench.transform.rotation = Quaternion.Lerp(interactableWorkbench.m_StartLerpRot, interactableWorkbench.m_TargetLerpTransform.rotation, interactableWorkbench.m_LerpPosTimer);
-                interactableWorkbench.transform.localScale = Vector3.Lerp(interactableWorkbench.m_StartLerpScale, interactableWorkbench.m_TargetLerpTransform.localScale, interactableWorkbench.m_LerpPosTimer);
-                if (interactableWorkbench.m_LerpPosTimer >= 1f)
+                if ((codes[i].opcode == OpCodes.Call || codes[i].opcode == OpCodes.Callvirt) &&
+                    codes[i].operand is MethodInfo method &&
+                    method == deltaTimeGetter)
                 {
-                    interactableWorkbench.m_LerpPosTimer = 0f;
-                    interactableWorkbench.m_IsLerpingToPos = false;
-                    interactableWorkbench.OnFinishLerp();
-                    if (interactableWorkbench.m_IsHideAfterFinishLerp)
+                    codes.InsertRange(i + 1, new List<CodeInstruction>
                     {
-                        interactableWorkbench.m_IsHideAfterFinishLerp = false;
-                        interactableWorkbench.gameObject.SetActive(false);
-                        return interactableWorkbench;
-                    }
-                }
-            }
-            else if (interactableWorkbench.m_IsMovingObject)
-            {
-                if (!interactableWorkbench.m_IsSnappingPos)
-                {
-                    interactableWorkbench.transform.position = Vector3.Lerp(interactableWorkbench.transform.position, interactableWorkbench.m_TargetMoveObjectPosition, Time.deltaTime * 7.5f);
-                }
-                interactableWorkbench.EvaluateSnapping();
-                int mask = LayerMask.GetMask(new string[]
-                {
-            "MoveStateBlockedArea",
-            "Customer"
-                });
-                Collider[] array = Physics.OverlapBox(interactableWorkbench.m_MoveStateValidArea.position, interactableWorkbench.m_MoveStateValidArea.lossyScale / 2f, interactableWorkbench.m_MoveStateValidArea.rotation, mask);
-                bool flag = true;
-                if (interactableWorkbench.m_PlaceObjectInShopOnly)
-                {
-                    int mask2 = LayerMask.GetMask(new string[]
-                    {
-                "MoveStateValidArea"
+                        new CodeInstruction(OpCodes.Call, speedMultiplierGetter),
+                        new CodeInstruction(OpCodes.Mul)
                     });
-                    if (Physics.OverlapBox(interactableWorkbench.m_MoveStateValidArea.position, interactableWorkbench.m_MoveStateValidArea.lossyScale / 2f, interactableWorkbench.m_MoveStateValidArea.rotation, mask2).Length == 0)
-                    {
-                        flag = false;
-                    }
-                }
-                else if (interactableWorkbench.m_PlaceObjectInWarehouseOnly)
-                {
-                    int mask3 = LayerMask.GetMask(new string[]
-                    {
-                "MoveStateValidWarehouseArea"
-                    });
-                    if (Physics.OverlapBox(interactableWorkbench.m_MoveStateValidArea.position, interactableWorkbench.m_MoveStateValidArea.lossyScale / 2f, interactableWorkbench.m_MoveStateValidArea.rotation, mask3).Length == 0)
-                    {
-                        flag = false;
-                    }
-                }
-                if (array.Length != 0 || interactableWorkbench.transform.position.y > 0.1f)
-                {
-                    flag = false;
-                }
-                if (interactableWorkbench.m_IsMovingObjectValidState != flag)
-                {
-                    interactableWorkbench.m_IsMovingObjectValidState = flag;
-                    ShelfManager.SetMoveObjectPreviewModelValidState(interactableWorkbench.m_IsMovingObjectValidState);
+
+                    i += 2;
+
+                    break;
                 }
             }
-            return interactableWorkbench;
+
+            return codes;
         }
     }
 }
--- FastWorkbench/FastWorkbench/Plugin.cs
+++ FastWorkbench/FastWorkbench/Plugin.cs
@@ -13,8 +13,9 @@ namespace FastWorkbench
         private static readonly Harmony Harmony = new(PluginInfo.PLUGIN_GUID);
         private static ManualLogSource Log { get; set; }
         public static ConfigEntry<bool> EnableMod { get; private set; }
+        public static ConfigEntry<float> MinimumPriceLimit { get; private set; }
         public static ConfigEntry<float> MaxPriceLimit { get; private set; }
-        public static ConfigEntry<int> SpeedMultiplier { get; private set; }
+        public static ConfigEntry<float> SpeedMultiplier { get; private set; }
         private void Awake()
         {
             Log = Logger;
@@ -22,21 +23,36 @@ namespace FastWorkbench
             EnableMod = Config.Bind("Fast Workbench",
                                     "Enable Mod",
                                     true,
-                                    new ConfigDescription("Enable this mod", null, new ConfigurationManagerAttributes { Order = 1 }));
+                                    new ConfigDescription("Enable this mod", null, new ConfigurationManagerAttributes { Order = 4 }));
+
+            MinimumPriceLimit = Config.Bind("Fast Workbench",
+                                         "Minimum price limit",
+                                         0.01f,
+                                         new ConfigDescription("Minimum price limit for workbench cards", new AcceptableValueRange<float>(0.01f, 20000f), new ConfigurationManagerAttributes { Order = 3 }));
 
             MaxPriceLimit = Config.Bind("Fast Workbench",
                                          "Max price limit",
-                                         1f,
-                                         new ConfigDescription("Maximum price limit for workbench cards", new AcceptableValueRange<float>(1, 1000), new ConfigurationManagerAttributes { Order = 1 }));
+                                         5f,
+                                         new ConfigDescription("Maximum price limit for workbench cards", new AcceptableValueRange<float>(0.01f, 20000f), new ConfigurationManagerAttributes { Order = 2 }));
 
             SpeedMultiplier = Config.Bind("Fast Workbench",
                                          "Speed multiplier",
-                                         1,
-                                         new ConfigDescription("Speed multiplier", new AcceptableValueRange<int>(1, 10), new ConfigurationManagerAttributes { Order = 1 }));
+                                         1f,
+                                         new ConfigDescription("Speed multiplier", new AcceptableValueRange<float>(1, 10), new ConfigurationManagerAttributes { Order = 1 }));
 
             MaxPriceLimit.SettingChanged += (_, _) =>
             {
                 MaxPriceLimit.Value = (float)Math.Round(MaxPriceLimit.Value, 2, MidpointRounding.AwayFromZero);
+            };
+
+            MinimumPriceLimit.SettingChanged += (_, _) =>
+            {
+                MinimumPriceLimit.Value = (float)Math.Round(MinimumPriceLimit.Value, 2, MidpointRounding.AwayFromZero);
+            };
+
+            SpeedMultiplier.SettingChanged += (_, _) =>
+            {
+                SpeedMultiplier.Value = (float)Math.Round(SpeedMultiplier.Value, 1, MidpointRounding.AwayFromZero);
             };
         }
 
@@ -52,9 +68,9 @@ namespace FastWorkbench
             L($"Plugin {PluginInfo.PLUGIN_NAME} is unloaded!");
         }
 
-        public static int SpeedMultiplierValue
+        public static float SpeedMultiplierValue
         {
-            get { return EnableMod.Value ? SpeedMultiplier.Value : 1; }
+            get { return EnableMod.Value ? SpeedMultiplier.Value : 1f; }
         }
 
         internal static void L(string message, bool info = false)
